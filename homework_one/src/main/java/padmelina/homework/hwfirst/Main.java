0a25fe0e4bf6b6f6471211db84d9364a945c24d1
padmelina <padmelina93@gmail.com>
2017-10-21 00:36:13 +0400

    First home work

---
+++ homework_one/src/main/java/padmelina/homework/hwfirst/Main.java
@@ -0,0 +1,22 @@
+package padmelina.homework.hwfirst;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
+public class Main {
+    public static void main(String[] args) {
+        double[][] matrixData = { {1d,2d,3d}, {2d,5d,3d}};
+        RealMatrix m = MatrixUtils.createRealMatrix(matrixData);
+
+        double[][] matrixData2 = { {1d,2d}, {2d,5d}, {1d, 7d}};
+        RealMatrix n = new Array2DRowRealMatrix(matrixData2);
+
+        RealMatrix p = m.multiply(n);
+        System.out.println(p.getRowDimension());    // 2
+        System.out.println(p.getColumnDimension()); // 2
+
+        RealMatrix pInverse = new LUDecomposition(p).getSolver().getInverse();
+    }
+}
