d2070ab3c1e5972ab3ae8a381ee195eb07f59f22
padmelina <padmelina93@gmail.com>
2017-11-04 13:02:08 +0300

    Fix incorrect sizes

--- homework_two/src/main/java/padmelina/homework/memory/MemoryCounter.java
+++ homework_two/src/main/java/padmelina/homework/memory/MemoryCounter.java
@@ -1,42 +1,40 @@
 package padmelina.homework.memory;
 
-import padmelina.homework.model.Constans;
 import padmelina.homework.model.MemoryAllocater;
 
-import java.util.Collection;
 import java.util.List;
 
-import static padmelina.homework.model.Constans.BIG_NUMBER_FOR_COLLECTION;
 import static padmelina.homework.model.Constans.BIG_NUMBER_FOR_MEMORY_CHECK;
 import static padmelina.homework.model.Constans.SMALLER_NUMBER_FOR_MEMORY_CHECK;
 
 public class MemoryCounter {
-    public static long countObjectSize(Class type) {
-        try {
-            MemoryUtils memoryUtils = new MemoryUtils();
-            memoryUtils.callGC();
-            long before = memoryUtils.getUsedMemory();
-            Object[] array = MemoryAllocater.createArray(BIG_NUMBER_FOR_MEMORY_CHECK, type);
-            long after = memoryUtils.getUsedMemory();
-            memoryUtils.callGC();
-            Object o = array[0];
-            return (after - before)/ BIG_NUMBER_FOR_MEMORY_CHECK;
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return 0;
+    public static long countObjectSize(Class type) throws InstantiationException, IllegalAccessException {
+        return countObjectSize(type, BIG_NUMBER_FOR_MEMORY_CHECK);
     }
 
-    public static long countCollectionSize(int sizeOfArray) throws IllegalAccessException, InstantiationException {
+    public static long countCollectionSize(int sizeOfCollection) throws IllegalAccessException, InstantiationException {
+        return countCollectionSize(SMALLER_NUMBER_FOR_MEMORY_CHECK, sizeOfCollection);
+    }
+
+    private static long countObjectSize(Class type, int sizeOfArray) throws IllegalAccessException, InstantiationException {
         MemoryUtils memoryUtils = new MemoryUtils();
         memoryUtils.callGC();
         long before = memoryUtils.getUsedMemory();
-        List<List<String>> array = MemoryAllocater.createCollection(SMALLER_NUMBER_FOR_MEMORY_CHECK, sizeOfArray);
+        Object[] array = MemoryAllocater.createArray(sizeOfArray, type);
+        long after = memoryUtils.getUsedMemory();
+        memoryUtils.callGC();
+        Object o = array[0];
+        return (after - before)/ sizeOfArray;
+    }
+
+    private static long countCollectionSize(int sizeOfArray, int sizeOfCollection) throws IllegalAccessException, InstantiationException {
+        MemoryUtils memoryUtils = new MemoryUtils();
+        memoryUtils.callGC();
+        long before = memoryUtils.getUsedMemory();
+        List<List<String>> array = MemoryAllocater.createCollection(sizeOfArray, sizeOfCollection);
         long after = memoryUtils.getUsedMemory();
         memoryUtils.callGC();
         array.size();
-        return (after - before)/ BIG_NUMBER_FOR_MEMORY_CHECK;
+        return (after - before)/ sizeOfArray;
     }
 }
